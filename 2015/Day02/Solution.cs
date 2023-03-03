bce821f698b485ba1cb306cb23c0b3aeef906be3
FaustVX <jodu035@gmail.com>
2023-03-03 17:30:06 +0100

    Solved P1 in 0:02:15

--- 2015/Day02/Solution.cs
+++ 2015/Day02/Solution.cs
@@ -4,10 +4,24 @@ namespace AdventOfCode.Y2015.Day02;
 [ProblemName("I Was Told There Would Be No Math")]
 public class Solution : Solver //, IDisplay
 {
-    public object PartOne(string input)
+    readonly record struct Box(int L, int W, int H)
     {
-        return 0;
+        private int Side1 => L*W;
+        private int Side2 => W*H;
+        private int Side3 => H*L;
+        public int Surface => 2*Side1 + 2*Side2 + 2*Side3 + Math.Min(Side1, Math.Min(Side2, Side3));
+        public static Box Parse(ReadOnlyMemory<char> input)
+        {
+            var parts = input.Split("x");
+            return new(int.Parse(parts.Span[0].Span), int.Parse(parts.Span[1].Span), int.Parse(parts.Span[2].Span));
+        }
     }
+    public object PartOne(string input)
+    => input.AsMemory()
+        .SplitLine()
+        .AsEnumerable()
+        .Select(Box.Parse)
+        .Sum(static box => box.Surface);
 
     public object PartTwo(string input)
     {
