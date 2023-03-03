fa13e738dfdbab03b9cd9943964afa0a4a84fea9
FaustVX <jodu035@gmail.com>
2023-03-03 19:36:38 +0100

    Initial commit for Y2015D5

---
+++ 2015/Day05/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2015.Day05;
+
+[ProblemName("Doesn't He Have Intern-Elves For This?")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
