e76b96004634dea70509e869ab6c544257c854bf
FaustVX <jodu035@gmail.com>
2023-03-03 17:27:51 +0100

    Initial commit for Y2015D2

---
+++ 2015/Day02/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2015.Day02;
+
+[ProblemName("I Was Told There Would Be No Math")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
