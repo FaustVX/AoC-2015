16e45c7bfa6a66f6f4a35a2421c9d852bb181c83
FaustVX <jodu035@gmail.com>
2023-03-03 17:52:22 +0100

    Solved P2 in 0:03:14

--- 2015/Day03/Solution.cs
+++ 2015/Day03/Solution.cs
@@ -37,6 +37,36 @@ public class Solution : Solver //, IDisplay
 
     public object PartTwo(string input)
     {
-        return 0;
+        var santa = (x: 0, y: 0);
+        var robot = (x: 0, y: 0);
+        var positions = new HashSet<(int x, int y)>
+        {
+            santa,
+        };
+        var isSanta = true;
+        foreach (var c in input)
+        {
+            ref var pos = ref isSanta ? ref santa : ref robot;
+            switch (c)
+            {
+                case '^':
+                    pos.y--;
+                    break;
+                case 'v':
+                    pos.y++;
+                    break;
+                case '<':
+                    pos.x--;
+                    break;
+                case '>':
+                    pos.x++;
+                    break;
+                default:
+                    throw new UnreachableException();
+            }
+            positions.Add(pos);
+            isSanta = !isSanta;
+        }
+        return positions.Count;
     }
 }
