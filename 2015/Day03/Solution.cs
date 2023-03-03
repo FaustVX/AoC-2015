ec0561e52d24e2dd36c7af186ba4363bee976519
FaustVX <jodu035@gmail.com>
2023-03-03 17:49:03 +0100

    Solved P1 in 0:04:34

--- 2015/Day03/Solution.cs
+++ 2015/Day03/Solution.cs
@@ -6,7 +6,33 @@ public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        var pos = (x: 0, y: 0);
+        var positions = new HashSet<(int x, int y)>
+        {
+            pos,
+        };
+        foreach (var c in input)
+        {
+            switch (c)
+            {
+                case '^':
+                    pos.y--;
+                    break;
+                case 'v':
+                    pos.y++;
+                    break;
+                case '<':
+                    pos.x--;
+                    break;
+                case '>':
+                    pos.x++;
+                    break;
+                default:
+                    throw new UnreachableException();
+            }
+            positions.Add(pos);
+        }
+        return positions.Count;
     }
 
     public object PartTwo(string input)
