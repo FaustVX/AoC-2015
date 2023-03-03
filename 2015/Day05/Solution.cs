7bfb24060ce59d19845435468447af6223ee0196
FaustVX <jodu035@gmail.com>
2023-03-03 19:59:10 +0100

    Solved P1 in 0:22:28

--- 2015/Day05/Solution.cs
+++ 2015/Day05/Solution.cs
@@ -6,7 +6,29 @@ public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        return input.AsMemory().SplitLine().AsEnumerable().Count(IsNice);
+
+        static bool IsNice(ReadOnlyMemory<char> input)
+        {
+            var vowelsCount = 0;
+            var hasDouble = false;
+            var hasForbidden = false;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input.Span[i] is 'a' or 'e' or 'i' or 'o' or 'u')
+                    vowelsCount++;
+                if (i >= 1)
+                {
+                    if (input.Span[i - 1] == input.Span[i])
+                        hasDouble = true;
+                    if (input.Span[(i - 1)..] is ['a', 'b', ..] or ['c', 'd', ..] or ['p', 'q', ..] or ['x', 'y', ..])
+                        hasForbidden = true;
+                }
+            }
+
+            return vowelsCount >= 3 && hasDouble && !hasForbidden;
+        }
     }
 
     public object PartTwo(string input)
