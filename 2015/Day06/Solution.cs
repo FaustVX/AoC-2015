a2fc4fdae8aab3713ca542e096e389dd045e5f32
FaustVX <jodu035@gmail.com>
2023-03-03 21:43:51 +0100

    Solved P2 in 0:08:24

--- 2015/Day06/Solution.cs
+++ 2015/Day06/Solution.cs
@@ -51,6 +51,50 @@ public class Solution : Solver //, IDisplay
 
     public object PartTwo(string input)
     {
-        return 0;
+        var light = new byte[1000, 1000];
+#pragma warning disable CS0612 //ObsoleteAttribute
+        foreach (var line in input.SplitLine())
+#pragma warning restore CS0612 //
+            switch (line.Split(" ,".ToCharArray()))
+            {
+                case ["toggle", var x1, var y1, _, var x2, var y2]:
+                    Toggle(light, int.Parse(x1), int.Parse(y1), int.Parse(x2), int.Parse(y2));
+                    break;
+                case [_, "on", var x1, var y1, _, var x2, var y2]:
+                    TurnOn(light, int.Parse(x1), int.Parse(y1), int.Parse(x2), int.Parse(y2));
+                    break;
+                case [_, "off", var x1, var y1, _, var x2, var y2]:
+                    TurnOff(light, int.Parse(x1), int.Parse(y1), int.Parse(x2), int.Parse(y2));
+                    break;
+                default:
+                    throw new UnreachableException();
+            }
+
+        return light.Cast<byte>().Sum(static l => l);
+
+        static void Toggle(byte[,] light, int x1, int y1, int x2, int y2)
+        {
+            for (var x = x1; x <= x2; x++)
+                for (var y = y1; y <= y2; y++)
+                    light[x, y] += 2;
+        }
+
+        static void TurnOn(byte[,] light, int x1, int y1, int x2, int y2)
+        {
+            for (var x = x1; x <= x2; x++)
+                for (var y = y1; y <= y2; y++)
+                    light[x, y]++;
+        }
+
+        static void TurnOff(byte[,] light, int x1, int y1, int x2, int y2)
+        {
+            for (var x = x1; x <= x2; x++)
+                for (var y = y1; y <= y2; y++)
+                {
+                    ref var l = ref light[x, y];
+                    if (l > 0)
+                        l--;
+                }
+        }
     }
 }
