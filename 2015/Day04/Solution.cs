9bb4152ca1dca9d52430a99f6c19670851a48127
FaustVX <jodu035@gmail.com>
2023-03-03 17:56:12 +0100

    Initial commit for Y2015D4

---
+++ 2015/Day04/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2015.Day04;
+
+[ProblemName("The Ideal Stocking Stuffer")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
