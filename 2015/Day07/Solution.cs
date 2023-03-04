7c37152ceaffae5fbe6557d120721dbfc91f6e2f
FaustVX <jodu035@gmail.com>
2023-03-04 13:01:34 +0100

    Initial commit for Y2015D7

---
+++ 2015/Day07/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2015.Day07;
+
+[ProblemName("Some Assembly Required")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
