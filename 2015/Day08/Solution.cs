6ca4627ff376759acf02276263adafd937656c1e
FaustVX <jodu035@gmail.com>
2023-03-05 17:05:17 +0100

    Solved P1 in 0:10:26

--- 2015/Day08/Solution.cs
+++ 2015/Day08/Solution.cs
@@ -6,7 +6,30 @@ public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        var totalSpace = 0;
+        foreach (var line in input.AsMemory().SplitLine().AsEnumerable())
+        {
+            var space = -1; // for the last "
+            for (var i = 1; i < line.Length; i++)
+            {
+                switch (line.Span[i..])
+                {
+                    case ['\\', 'x', ..]:
+                        space += 1;
+                        i += 3;
+                        break;
+                    case ['\\', ..]:
+                        space += 1;
+                        i += 1;
+                        break;
+                    default:
+                        space++;
+                        break;
+                }
+            }
+            totalSpace += line.Length - space;
+        }
+        return totalSpace;
     }
 
     public object PartTwo(string input)
