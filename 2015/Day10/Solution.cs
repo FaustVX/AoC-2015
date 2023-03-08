c98d3941ba3509ac09c73a4d5ff60e6435715adb
FaustVX <jodu035@gmail.com>
2023-03-08 19:49:12 +0100

    Solved P1 in 0:48:39

--- 2015/Day10/Solution.cs
+++ 2015/Day10/Solution.cs
@@ -6,7 +6,26 @@ public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        for (var round = 0; round < 40; round++)
+        {
+            var sb = new StringBuilder();
+            var last = input[0];
+            var count = 1;
+            for (var i = 1; i < input.Length; i++)
+            {
+                if (input[i] == last)
+                    count++;
+                else
+                {
+                    sb.Append(count).Append(last);
+                    last = input[i];
+                    count = 1;
+                }
+            }
+            sb.Append(count).Append(last);
+            input = sb.ToString();
+        }
+        return checked(input.Length);
     }
 
     public object PartTwo(string input)
