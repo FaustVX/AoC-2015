627688251904d6e14304bc66b444e29cea197617
FaustVX <jodu035@gmail.com>
2023-03-08 20:03:28 +0100

    Initial commit for Y2015D11

---
+++ 2015/Day11/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2015.Day11;
+
+[ProblemName("Corporate Policy")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
