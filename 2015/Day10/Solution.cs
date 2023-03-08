6430e2e55e289e79bda3679bfb7b2bb339980d86
FaustVX <jodu035@gmail.com>
2023-03-08 19:36:41 +0100

    Y2015D10P1

--- 2015/Day10/Solution.cs
+++ 2015/Day10/Solution.cs
@@ -6,7 +6,30 @@ public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        for (int i = 0; i < 40; i++)
+            input = LookandSay(input);
+        return input.Length;
+    }
+
+    private static string LookandSay(string input)
+    {
+        return LookandSay(input.AsSpan(), new()).ToString();
+
+        static StringBuilder LookandSay(ReadOnlySpan<char> input, StringBuilder output)
+        {
+            if (input.Length == 1)
+                return output.Append($"1{input[0]}");
+            if (input.Length == 0)
+                return output;
+            var i = 1;
+            var count = 1;
+            for (; i < input.Length; i++)
+                if (input[i] == input[0])
+                    count++;
+                else
+                    return LookandSay(input[i..], output.Append($"{count}{input[0]}"));
+            return output;
+        }
     }
 
     public object PartTwo(string input)
