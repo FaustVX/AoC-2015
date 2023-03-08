8e33159a1422073cc951275216f237df7239ad5c
FaustVX <jodu035@gmail.com>
2023-03-08 19:00:29 +0100

    Initial commit for Y2015D10

---
+++ 2015/Day10/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2015.Day10;
+
+[ProblemName("Elves Look, Elves Say")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
