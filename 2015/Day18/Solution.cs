5525ebdbc7d31e70b0d2da837b642bd4df33c677
FaustVX <jodu035@gmail.com>
2023-03-10 19:28:56 +0100

    Initial commit for Y2015D18

---
+++ 2015/Day18/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2015.Day18;
+
+[ProblemName("Like a GIF For Your Yard")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
