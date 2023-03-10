faa96f9c0712e008caf924aea0d8535f2da0e791
FaustVX <jodu035@gmail.com>
2023-03-10 22:13:19 +0100

    Solved P2 in 1:31:55

--- 2015/Day18/Solution.cs
+++ 2015/Day18/Solution.cs
@@ -12,24 +12,30 @@ public class Solution : Solver //, IDisplay
         var size = light.GetLength(0);
         var steps = Globals.IsTestInput ? 4 : 100;
         for (int step = 0; step < steps; step++)
-            NextStep(ref light, ref bak);
+            NextStep(ref light, ref bak, setCorner: false);
         return Count(light);
     }
 
-    private static void NextStep(ref bool[,] light, ref bool[,] next)
+    private static void NextStep(ref bool[,] light, ref bool[,] next, bool setCorner)
     {
-        ReadOnlySpan2D<bool> span = light;
-        var size = span.Width;
-        for (int x = 0; x < size; x++)
-            for (int y = 0; y < size; y++)
-            {
-                next[x, y] = (light[x, y], Count(Slice3x3(span, x, y))) switch
-                {
-                    (true, var count) => (count - 1) is 2 or 3, // -1 to remove light[x, y] (which is on)
-                    (false, var count) => count is 3,
-                };
-            }
+        Inner(light, next, setCorner);
         (light, next) = (next, light);
+
+        static void Inner(ReadOnlySpan2D<bool> light, Span2D<bool> next, bool setCorner)
+        {
+            var size = light.Width;
+
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    next[x, y] = (light[x, y], Count(Slice3x3(light, x, y))) switch
+                    {
+                        (true, var count) => (count - 1) is 2 or 3, // -1 to remove light[x, y] (which is on)
+                        (false, var count) => count is 3,
+                    };
+
+            if (setCorner)
+                next[0, 0] = next[0, size - 1] = next[size - 1, 0] = next[size - 1, size - 1] = true;
+        }
 
         static ReadOnlySpan2D<bool> Slice3x3(ReadOnlySpan2D<bool> span, int x, int y)
         {
@@ -74,6 +80,13 @@ public class Solution : Solver //, IDisplay
 
     public object PartTwo(string input)
     {
-        return 0;
+        var (light, bak) = ParseInput(input.AsMemory().SplitLine());
+        var size = light.GetLength(0);
+        var steps = Globals.IsTestInput ? 5 : 100;
+
+        light[0, 0] = light[0, size - 1] = light[size - 1, 0] = light[size - 1, size - 1] = true;
+        for (int step = 0; step < steps; step++)
+            NextStep(ref light, ref bak, setCorner: true);
+        return Count(light);
     }
 }
