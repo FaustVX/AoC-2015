f0f69ae9ae8e107a83b9c12a3ee8fbcd93508ebc
FaustVX <jodu035@gmail.com>
2023-03-10 13:49:14 +0100

    Initial commit for Y2015D17

---
+++ 2015/Day17/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2015.Day17;
+
+[ProblemName("No Such Thing as Too Much")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
