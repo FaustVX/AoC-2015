bad85db93fdd21e820961b019663b7b0884f15a2
FaustVX <jodu035@gmail.com>
2023-03-12 00:03:11 +0100

    Initial commit for Y2015D21

---
+++ 2015/Day21/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2015.Day21;
+
+[ProblemName("RPG Simulator 20XX")]
+public class Solution : Solver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
