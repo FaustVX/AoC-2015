65de874659f490db02e764c298a98bf0797e714d
FaustVX <jodu035@gmail.com>
2023-03-11 23:26:15 +0100

    Solved P1 in 3:28:35

--- 2015/Day20/Solution.cs
+++ 2015/Day20/Solution.cs
@@ -6,7 +6,24 @@ public class Solution : Solver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        var requestedPresents = int.Parse(input);
+        var house = 1;
+        while (SumOfPresents(house) < requestedPresents)
+            house += 1;
+        return house;
+    }
+
+    private static int SumOfPresents(int house)
+    {
+        var sum = 0;
+        var d = (int)Math.Sqrt(house) + 1;
+        for (var elf = 1; elf <= d; elf++)
+            if (house % elf == 0)
+            {
+                sum += elf;
+                sum += house / elf;
+            }
+        return sum * 10;
     }
 
     public object PartTwo(string input)
